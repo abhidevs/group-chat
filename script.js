45ad8ed85d3363aec542a72828722c015992dbb3
Abhik Das <abhikdas98765@gmail.com>
2022-05-08 17:59:27 +0530

    integrated signup page with backend

---
+++ script.js
@@ -0,0 +1,25 @@
+const signUpForm = document.getElementById("signupForm");
+
+const backendAPI = "http://localhost:3000/api";
+
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(signUpForm);
+
+  let name = formData.get("name");
+  let email = formData.get("email");
+  let phone = formData.get("phone");
+  let password = formData.get("password");
+
+  axios
+    .post(`${backendAPI}/auth/signup`, { name, email, phone, password })
+    .then((res) => {
+      alert("Registered successfully");
+      signUpForm.reset();
+      console.log(res.data);
+    })
+    .catch((err) => {
+      alert(err.response.data.message);
+      console.log(err.response);
+    });
+});
